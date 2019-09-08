fe3883668519e5f295970fc34f113c98b9071bfd
teesmart <42120584+teesmart@users.noreply.github.com>
2019-09-08 22:41:37 +0100

    Add files via upload

---
+++ Tosin.js
@@ -0,0 +1,18 @@
+<script text/javascript>
+function validateform()
+{
+var name=document.form.name.value;
+var E-mail=document.form.E-mail.value;
+var password=document.form.password.value;
+
+
+
+if (name.length<4 ){alert("characters should exceed 4");
+return false;
+}
+else if(message.length<20){alert("character should not exceed 20");
+return false;
+}
+
+}
+</script>
